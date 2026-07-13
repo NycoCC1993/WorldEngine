92445b45e800d435ee022a5300378602e6216268
NycoCC <faerenh@gmail.com>
2026-07-12 23:10:58 -0400

    Add the Gravity files & regulators folder

---
+++ Regulators/Gravity.cpp
@@ -0,0 +1,13 @@
+// The purpose of Gravity.cpp is to hold functions that will provide data to objects which require
+// a gravity action to be performed on them.
+
+#include "Gravity.h"
+
+class Gravity
+{
+    // Constructor for the Gravity Handler
+    Gravity::Gravity()
+    {
+        //
+    }
+};
---
+++ Regulators/Gravity.h
@@ -0,0 +1,13 @@
+#ifndef GRAVITY_H
+#define GRAVITY_H
+
+class Gravity
+{
+    public:
+        const double GRAVITY_CONSTANT = 9.8;
+
+    private:
+        double gravityModifier = 1.0;
+};
+
+#endif // GRAVITY_H
