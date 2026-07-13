4cd0263cc46454f70ed3f572c726cbbab0a94fe7
NycoCC <faerenh@gmail.com>
2026-07-12 23:12:04 -0400

    Apparently you need to add all the files every time, whoda thunk it

--- Executor.cpp
+++ Executor.cpp
@@ -1,6 +1,13 @@
+// Executor for the physics engine, this is the main entry point for the program.
+// Code should be controlled from here. We also can put an overarching state machine to advance a frame.
+
+// C++ includes
 #include <iostream>
 
+// Project includes
+#include "Regulators\Gravity.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "NYCO Computing Company \"worldEngine\" under the creative commons license, 2026" << std::endl;
     return 0;
 }
