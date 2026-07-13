52bf30ef9d868d9e04cbdcd9b9c9e20551c7f2c5
NycoCC <faerenh@gmail.com>
2026-07-13 16:49:25 -0400

    Add the support for calculations for physics

--- Regulators/EntityController.cpp
+++ Regulators/EntityController.cpp
@@ -1,1 +1,21 @@
 // The purpose of the Entity Controller is to handle all of the physics objects and make sure their needs are met
+
+#include "EntityController.h"
+
+// Execute the needed physics functions for each object in the list. Expect to run once per frame maybe
+bool EntityController::runPhysics()
+{
+    for (PhysicsObject* each : this->priorityObjects)
+    {
+        if (each->getIs3dObject())
+        {
+            // perform any 3D-specific calculations
+        }
+        else
+        {
+            // perform any 2D-specific calculations
+        }
+
+        // generic physics calculations
+    }
+}
--- Regulators/EntityController.h
+++ Regulators/EntityController.h
@@ -9,7 +9,12 @@ class EntityController
     public:
         std::vector<PhysicsObject*> priorityObjects; // objects that need to be executed first
         std::vector<PhysicsObject*> objects;
+        bool runPhysics();
     private:
+        Gravity universalGravity;
+        std::vector<double> calculateNewPosition(std::vector<double> oldPos);
+        std::vector<double> calculateNewVelocityVector(std::vector<double> oldVel);
+        std::vector<double> calculateNewAcceleration(std::vector<double> oldAcc);
 };
 
 #endif //ENTITYCONTROLLER_H
